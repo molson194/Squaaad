820037bced8fadc3865bc249d9b9152b2c4c5149
Matthew Olson <Olson@matthews-macbook-air-2.local>
2015-11-05 00:21:41 -0500

    Initial Commit

---
+++ Squaaad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Squaaad
+//
+//  Created by Matthew Olson on 11/5/15.
+//  Copyright © 2015 Molson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Squaaad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Squaaad
+//
+//  Created by Matthew Olson on 11/5/15.
+//  Copyright © 2015 Molson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
