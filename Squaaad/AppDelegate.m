cf1c2c6952c871cc1b87b769da9c183636935406
Matthew Olson <Olson@matthews-macbook-air-2.local>
2015-11-05 00:52:04 -0500

    Setting up Parse Integration

--- Squaaad/AppDelegate.m
+++ Squaaad/AppDelegate.m
@@ -6,6 +6,7 @@
 //  Copyright © 2015 Molson. All rights reserved.
 //
 
+#import <Parse/Parse.h>
 #import "AppDelegate.h"
 #import "ViewController.h"
 
@@ -17,6 +18,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [Parse setApplicationId:@"ReW1u28NMNWhX09YCBbz8q90elYRrrFTmrw3fAjl"
+                  clientKey:@"tlP8LJ13zlPwq5jQREWZfTiGF4ovMMLD0vaQsU57"];
+    
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    testObject[@"foo"] = @"bar";
+    [testObject saveInBackground];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
