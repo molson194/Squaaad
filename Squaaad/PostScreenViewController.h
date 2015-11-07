2ac7c0ac72a679f0f73244036cce3d689dada2b0
Matt Levey <mattlevey96@gmail.com>
2015-11-07 12:46:52 -0500

    button to new poster screen

---
+++ Squaaad/PostScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostScreenViewController.h
+//  Squaaad
+//
+//  Created by Matthew Levey on 11/7/15.
+//  Copyright © 2015 Molson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostScreenViewController : UIViewController
+
+@end
