c3a710fc8b2f02e1c3da14863c3b27051698bb8e
Matthew Olson <Olson@matthews-macbook-air-2.local>
2015-11-07 19:26:37 -0500

    Location lat/lon and text input

--- Squaaad/PostScreenViewController.h
+++ Squaaad/PostScreenViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface PostScreenViewController : UIViewController <UITextViewDelegate>
+@interface PostScreenViewController : UIViewController <UITextViewDelegate, CLLocationManagerDelegate>
 
 @end
